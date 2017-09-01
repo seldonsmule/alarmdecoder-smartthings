28c5da1cea25de2302dd46bdc2e7c6c74bc02002
Eric Cunningham <eric@c-the-world.org>
2017-09-01 13:46:54 -0400

    Delete create_cert.sh

--- alarmdecoder/scripts/create_cert.sh
+++
@@ -1,9 +0,0 @@
-openssl req \
-    -new \
-    -newkey rsa:4096 \
-    -days 3650 \
-    -nodes \
-    -x509 \
-    -subj "/C=US/ST=VA/L=Vienta/O=CTW/CN=localhost" \
-    -keyout server.key \
-    -out server.crt
