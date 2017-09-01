eb471bb745f1911aef60fd9d05e0f912bf47e2ee
Eric Cunningham <eric@c-the-world.org>
2017-09-01 13:47:48 -0400

    Delete env_profile.sh

--- alarmdecoder/scripts/env_profile.sh
+++
@@ -1,7 +0,0 @@
-set -o vi
-
-# for mysql and all the MAMP installed stuff
-#PATH="/Applications/MAMP/Library/bin"
-
-TMPPATH=$PATH
-PATH="/Applications/MAMP/Library/bin":"/Applications/MAMP/bin/php/php7.1.6/bin":$PATH
